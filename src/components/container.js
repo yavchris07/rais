01a9270b82b098fd85b7d0a881527f447ae896d7
the thunder <wilondjayav27@gamil.com>
2023-05-11 10:01:11 +0100

    second new commit on 11th may

--- src/components/container.js
+++ src/components/container.js
@@ -7,10 +7,10 @@ export default function Container() {
         <div className='container'>
             Vous êtes un étudiant en informatique, le code vous semble difficile. 
             La méthode d’analyse informatique vous complique. L’analyse vous surpasse. 
-            La réalisation d’un logiciel, d’un site web moderne, une application mobile parait un mystere.
+            La réalisation d’un logiciel, d’un site web moderne, une application mobile parait un mystère.
             Raïs est là pour vous accompagner tout au long de votre belle année terminale; 
             en rendant le code facile, la méthode d’analyse informatique moins compliquée, 
-            l’analyse informatique un jeu d’enfants. L'algorithme, etc.
+            l’analyse informatique un jeu d’enfants. L'algorithme:cet ensemble des règles opératoires propres à un calcul,vous nuise l'esprit ?
         </div>
     )
 }
